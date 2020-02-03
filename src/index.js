fa6bd213ebc445c6850b828b53a366dd49fb6604
Chris Hundhausen <chundhau@gmail.com>
2020-02-03 08:39:32 -0800

    initialStaticApp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './styles/index.css';
+import App from './components/App.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
