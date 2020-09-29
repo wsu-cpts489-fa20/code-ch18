84ef8869c2dc1d694c13bd779f995b07a5bec4ff
Chris Hundhausen <hundhaus@wsu.edu>
2020-09-29 11:19:33 -0700

    Added menuOpen state and state change methods into app and integrated them  into props of child components

--- src/components/App.js
+++ src/components/App.js
@@ -10,25 +10,51 @@ class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {mode: AppMode.LOGIN};
+    this.state = {mode: AppMode.LOGIN,
+                  menuOpen: false};
   }
 
   handleChangeMode = (newMode) => {
     this.setState({mode: newMode});
   }
 
+  openMenu = () => {
+    this.setState({menuOpen : true});
+  }
+  
+  closeMenu = () => {
+    this.setState({menuOpen : false});
+  }
+
+  toggleMenuOpen = () => {
+    this.setState(prevState => ({menuOpen: !prevState.menuOpen}));
+  }
+
+
   render() {
     return (
-    <div>
-      <NavBar title="Welcome to SpeedScore" mode={this.state.mode}/>
-      <SideMenu mode={this.state.mode}/>
-      <ModeBar mode={this.state.mode} 
-               changeMode={this.handleChangeMode}/>
-      <FloatingButton mode={this.state.mode}/>
-      <LoginPage changeMode={this.handleChangeMode}/>
-    </div>
-    );
-  }
+      <div>
+        <NavBar title="Welcome to SpeedScore" 
+          mode={this.state.mode}
+          changeMode={this.handleChangeMode}
+          menuOpen={this.state.menuOpen}
+          toggleMenuOpen={this.toggleMenuOpen}/>
+        <SideMenu 
+          mode={this.state.mode}
+          menuOpen={this.state.menuOpen}
+          changeMode={this.handleChangeMode}/>
+        <ModeBar 
+          mode={this.state.mode} 
+          changeMode={this.handleChangeMode}
+          menuOpen={this.state.menuOpen}/>
+        <FloatingButton 
+          mode={this.state.mode}
+          menuOpen={this.state.menuOpen}/>
+        <LoginPage changeMode={this.handleChangeMode}/>
+      </div>
+      );  
+}
+
 
 }
 
