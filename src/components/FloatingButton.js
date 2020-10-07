15329ab40b375692ad561de8d0294713c0513f41
Chris Hundhausen <hundhaus@wsu.edu>
2020-10-07 16:29:35 -0700

    Updated FloatingButton.js so that onClick() is defined for <div> instead of <a>. Removed <a> because it isn't necessary.

--- src/components/FloatingButton.js
+++ src/components/FloatingButton.js
@@ -3,10 +3,8 @@ import React from 'react';
 class FloatingButton extends React.Component {
     render() {
       return(
-        <div className="floatbtn">
-          <a onClick={this.props.handleClick}>
-            <span className="floatbtn-icon fa fa-plus"></span>
-          </a>
+        <div className="floatbtn" onClick={this.props.handleClick}>
+          <span className="floatbtn-icon fa fa-plus"></span>
         </div>  
       );
     }
