dd1de509b4c68a22e2916316315d9f066806fdb3
Chris Hundhausen <hundhaus@wsu.edu>
2020-10-08 12:06:58 -0700

    Made "Rounds" button on ModeBar component highlight when mode is either ROUNDS, ROUNDS_EDITROUND or ROUNDS_LOGROUND

--- src/components/ModeBar.js
+++ src/components/ModeBar.js
@@ -12,7 +12,10 @@ class ModeBar extends React.Component {
           <span className="modebaricon fa fa-th-list"></span>
           <span className="modebar-text">Feed</span>
         </a>
-        <a className={(this.props.mode === AppMode.ROUNDS ? " item-selected" : null)}
+        <a className={(this.props.mode === AppMode.ROUNDS || 
+               this.props.mode === AppMode.ROUNDS_EDITROUND || 
+               this.props.mode === AppMode.ROUNDS_LOGROUND ? 
+                  " item-selected" : null)}
            onClick={()=>this.props.changeMode(AppMode.ROUNDS)}>
           <span className="modebar-icon  fa fa-history"></span>
           <span className="modebar-text">Rounds</span>
